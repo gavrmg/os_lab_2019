2f8ed3f75249a14e1b5361f5a003a4683801f4d7
gavrmg <gavrmeh@gmail.com>
2020-02-29 08:54:47 +0000

    YOLO LAB2
     Please enter the commit message for your changes. Lines starting

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,23 @@
 #include "revert_string.h"
-
+#include "string.h"
 void RevertString(char *str)
 {
-	// your code here
+    int length, c;
+    char *begin, *end, temp;
+    length = strlen(str);
+    begin = str;
+    end = str + length - 1;
+
+    
+    for (c = 0; c < length/2; c++)
+    {
+        temp = *end;
+        *end = *begin;
+        *begin = temp;
+
+        begin++;
+        end--;
+    }
+
 }
 
